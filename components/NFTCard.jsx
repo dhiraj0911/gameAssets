8c42d7ff878cbd2c72d1487cbb1c0ad0fc47bbf6
dhiraj0911 <98685837+dhi-raj07@users.noreply.github.com>
2023-11-30 15:43:03 +0530

    nft cards

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -1,38 +1,69 @@
-import { useContext } from 'react';
-import Image from 'next/image';
+// import { useContext } from 'react';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { NFTContext } from '../context/NFTContext';
+
+// import images from '../assets';
+// // import { shortenAddress } from '../utils/shortenAddress';
+
+// const NFTCard = ({ nft, onProfilePage }) => {
+//   const { nftCurrency } = useContext(NFTContext);
+//   return (
+//     <Link href={{ pathname: '/nft-details', query: nft }}>
+
+//       <div className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md">
+//         <div className="relative w-full h-52 sm:h-36 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden">
+//           {/* <Image
+//             src={nft.image || images[`nft${nft.i}`]}
+//             layout="fill"
+//             objectFit="cover"
+//             alt={`nft-${nft.i}`}
+//           /> */}
+//           <h1>{nft.name}</h1>
+//         </div>
+//         <div className="mt-3 flex flex-col ">
+//           <p className="font-poppins ...">{nft.name}</p>
+//             {/* Display additional information */}
+//             <p className="font-poppins ...">ID: {nft.id}</p>
+//             {nft.forSale && (
+//               <p className="font-poppins ...">Price: {nft.price} {nftCurrency}</p>
+//             )}
+//             {nft.forRent && (
+//               <p className="font-poppins ...">Rent Price: {nft.rentPrice} {nftCurrency}</p>
+//             )}
+//             {/* View More Button */}
+//             <button className="mt-2 ...">View More</button>
+//         </div>
+//       </div>
+
+//     </Link>
+//   );
+// };
+
+// export default NFTCard;
+
+import React, { useContext } from 'react';
 import Link from 'next/link';
 import { NFTContext } from '../context/NFTContext';
-
-import images from '../assets';
-// import { shortenAddress } from '../utils/shortenAddress';
 
 const NFTCard = ({ nft, onProfilePage }) => {
   const { nftCurrency } = useContext(NFTContext);
   return (
     <Link href={{ pathname: '/nft-details', query: nft }}>
 
-      <div className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md">
-        <div className="relative w-full h-52 sm:h-36 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden">
-          {/* <Image
-            src={nft.image || images[`nft${nft.i}`]}
-            layout="fill"
-            objectFit="cover"
-            alt={`nft-${nft.i}`}
-          /> */}
-          <h1>{nft.name}</h1>
-        </div>
-        <div className="mt-3 flex flex-col ">
-          <p className="font-poppins ...">{nft.name}</p>
-            {/* Display additional information */}
-            <p className="font-poppins ...">ID: {nft.id}</p>
-            {nft.forSale && (
-              <p className="font-poppins ...">Price: {nft.price} {nftCurrency}</p>
-            )}
-            {nft.forRent && (
-              <p className="font-poppins ...">Rent Price: {nft.rentPrice} {nftCurrency}</p>
-            )}
-            {/* View More Button */}
-            <button className="mt-2 ...">View More</button>
+      <div className="w-[11.875em] h-[15.875em] p-4 bg-white bg-opacity-10 border border-white border-opacity-20 rounded-lg transition-all duration-300 ease-in-out hover:shadow-[0_0_20px_1px_#ffbb763f] hover:border-opacity-45">
+        <h1 className="text-2xl font-semibold tracking-wide">{nft.name}</h1>
+        <div>
+          <div>
+            <p>ID: {nft.id}</p>
+            {/* {nft.forSale && (
+              <p>Price: {nft.price} {nftCurrency}</p>
+            )} */}
+            {/* {nft.forRent && (
+              <p>Rent Price: {nft.rentPrice} {nftCurrency}</p>
+            )} */}
+            {/* <button>View More</button> */}
+          </div>
         </div>
       </div>
 
--- test/tests.js
+++ test/tests.js
@@ -32,114 +32,114 @@ describe("RentableNFTMarketplace", function () {
   });
 
   describe("Marketplace", function () {
-    // it("Should create and execute market sales", async function () {
-    //   // Seller lists an item
-    //   await marketplace.connect(seller).createToken("tokenURI", ethers.utils.parseEther("1"), ethers.utils.parseEther("0.01"), true, true, false, { value: listingPrice });
-    //   const newTokenId = await marketplace._tokenIds();
+    it("Should create and execute market sales", async function () {
+      // Seller lists an item
+      await marketplace.connect(seller).createToken("tokenURI", ethers.utils.parseEther("1"), ethers.utils.parseEther("0.01"), true, true, false, { value: listingPrice });
+      const newTokenId = await marketplace._tokenIds();
 
-    //   // Buyer purchases an item
-    //   await marketplace.connect(buyer).createMarketSale(newTokenId, { value: ethers.utils.parseEther("1") });
-    //   const item = await marketplace.idToMarketItem(newTokenId);
+      // Buyer purchases an item
+      await marketplace.connect(buyer).createMarketSale(newTokenId, { value: ethers.utils.parseEther("1") });
+      const item = await marketplace.idToMarketItem(newTokenId);
 
-    //   expect(item.owner).to.equal(buyer.address);
-    //   expect(item.sold).to.be.true;
-    // });
+      expect(item.owner).to.equal(buyer.address);
+      expect(item.sold).to.be.true;
+    });
 
-    // it("Should allow users to resell tokens", async function () {
-    //     // Seller lists an item
-    //     await marketplace.connect(seller).createToken("tokenURI", ethers.utils.parseEther("0.1"), ethers.utils.parseEther("0.001"), true, true, false, { value: listingPrice });
-        
-    //     const newTokenId = await marketplace._tokenIds();
-      
-    //     await marketplace.connect(buyer).createMarketSale(newTokenId, { value: ethers.utils.parseEther("0.1") });
-    //     let item = await marketplace.idToMarketItem(newTokenId);
-        
-    //     expect(item.owner).to.equal(buyer.address);
-    //     expect(item.price).to.equal(ethers.utils.parseEther("0.1"));
-    //     console.log(seller.address, item.owner, buyer.address, owner.address);
-
-    //     // Buyer resells the item
-    //     await marketplace.connect(buyer).resellToken(newTokenId, ethers.utils.parseEther("1"), ethers.utils.parseEther("0.01"), true, true, false, { value: listingPrice });
-    //     let newItem = await marketplace.idToMarketItem(newTokenId);
-      
-    //     console.log(seller.address, newItem.owner, buyer.address, owner.address);
-    //     // expect(newItem.owner).to.equal(marketplaceAddress.address);  // Check against buyer, not owner
-    //     expect(newItem.price).to.equal(ethers.utils.parseEther("1"));  // Check newItem's price
-    //     expect(newItem.rentPrice).to.equal(ethers.utils.parseEther("0.01"));  // Check newItem's rentPrice
-    // });
-
-    // it("Should not allow users to rent out tokens after expiry", async function () {
-    //     // Seller lists an item for rent
-    //     const rentPrice = ethers.utils.parseEther("0.001"); // Rent price set by the seller
-    //     await marketplace.connect(seller).createToken("tokenURI", ethers.utils.parseEther("0.1"), rentPrice, true, true, false, { value: listingPrice });
-    //     const newTokenId = await marketplace._tokenIds();
-      
-    //     // Set the expiration time for the rent
-    //     const rentDuration = 24 * 60 * 60; // 24 hours in seconds
-    //     const expires = (await ethers.provider.getBlock('latest')).timestamp + rentDuration;
-      
-    //     // Renter rents the item
-    //     await marketplace.connect(renter).rentOutToken(newTokenId, expires, { value: rentPrice });
-
-    //     // Check if the token is rented out correctly
-    //     let userOfToken = await marketplace.userOf(newTokenId);
-    //     expect(userOfToken).to.equal(renter.address);
-      
-    //     // Advance the blockchain time by more than 24 hours to simulate expiry
-    //     await ethers.provider.send('evm_increaseTime', [rentDuration + 1]); // +1 to ensure we're past the expiry
-    //     await ethers.provider.send('evm_mine'); // mine a new block to make sure the time change takes effect
-      
-    //     userOfToken = await marketplace.userOf(newTokenId);
-    //     expect(userOfToken).to.equal(ethers.constants.AddressZero, "The token should not have an active user after expiry");
-    //   });
-
-    // it("Should allow users to rent out tokens", async function () {
-    //     // Seller lists an item for rent
-    //     const rentPrice = ethers.utils.parseEther("0.001"); // Rent price set by the seller
-    //     await marketplace.connect(seller).createToken("tokenURI", ethers.utils.parseEther("0.1"), rentPrice, true, true, false, { value: listingPrice });
-    //     const newTokenId = await marketplace._tokenIds();
-      
-    //     // Set the expiration time for the rent
-    //     const rentDuration = 24 * 60 * 60; // 24 hours in seconds
-    //     const expires = (await ethers.provider.getBlock('latest')).timestamp + rentDuration;
-      
-    //     // Renter rents the item
-    //     await marketplace.connect(renter).rentOutToken(newTokenId, expires, { value: rentPrice });
-
-    //     // Check if the token is rented out correctly
-    //     let userOfToken = await marketplace.userOf(newTokenId);
-    //     expect(userOfToken).to.equal(renter.address);
-      
-    //     // Advance the blockchain time by less than 24 hours to simulate expiry
-    //     await ethers.provider.send('evm_increaseTime', [rentDuration - 1]); // +1 to ensure we're past the expiry
-    //     await ethers.provider.send('evm_mine'); // mine a new block to make sure the time change takes effect
-      
-    //     userOfToken = await marketplace.userOf(newTokenId);
-    //     expect(userOfToken).to.equal(renter.address, "The token should have an active user before expiry");
-    //   });
-
-      it("Should fetch and return all available NFTs for sale", async function () {
+    it("Should allow users to resell tokens", async function () {
         // Seller lists an item
         await marketplace.connect(seller).createToken("tokenURI", ethers.utils.parseEther("0.1"), ethers.utils.parseEther("0.001"), true, true, false, { value: listingPrice });
-        const newTokenId1 = await marketplace._tokenIds();
-
-        await marketplace.connect(seller).createToken("tokenURI2", ethers.utils.parseEther("0.1"), ethers.utils.parseEther("0.001"), true, true, false, { value: listingPrice });
-        const newTokenId2 = await marketplace._tokenIds();
+        
+        const newTokenId = await marketplace._tokenIds();
       
-        await marketplace.connect(seller).createToken("tokenURI2", ethers.utils.parseEther("0.1"), ethers.utils.parseEther("0.001"), true, true, false, { value: listingPrice });
-        // const newTokenId2 = await marketplace._tokenIds();
+        await marketplace.connect(buyer).createMarketSale(newTokenId, { value: ethers.utils.parseEther("0.1") });
+        let item = await marketplace.idToMarketItem(newTokenId);
+        
+        expect(item.owner).to.equal(buyer.address);
+        expect(item.price).to.equal(ethers.utils.parseEther("0.1"));
+        console.log(seller.address, item.owner, buyer.address, owner.address);
+
+        // Buyer resells the item
+        await marketplace.connect(buyer).resellToken(newTokenId, ethers.utils.parseEther("1"), ethers.utils.parseEther("0.01"), true, true, false, { value: listingPrice });
+        let newItem = await marketplace.idToMarketItem(newTokenId);
       
+        console.log(seller.address, newItem.owner, buyer.address, owner.address);
+        // expect(newItem.owner).to.equal(marketplaceAddress.address);  // Check against buyer, not owner
+        expect(newItem.price).to.equal(ethers.utils.parseEther("1"));  // Check newItem's price
+        expect(newItem.rentPrice).to.equal(ethers.utils.parseEther("0.01"));  // Check newItem's rentPrice
+    });
+
+    it("Should not allow users to rent out tokens after expiry", async function () {
+        // Seller lists an item for rent
+        const rentPrice = ethers.utils.parseEther("0.001"); // Rent price set by the seller
+        await marketplace.connect(seller).createToken("tokenURI", ethers.utils.parseEther("0.1"), rentPrice, true, true, false, { value: listingPrice });
+        const newTokenId = await marketplace._tokenIds();
       
-        // Buyer purchases an item
-        await marketplace.connect(buyer).createMarketSale(newTokenId1, { value: ethers.utils.parseEther("0.1") });
-        const item = await marketplace.idToMarketItem(newTokenId1);
+        // Set the expiration time for the rent
+        const rentDuration = 24 * 60 * 60; // 24 hours in seconds
+        const expires = (await ethers.provider.getBlock('latest')).timestamp + rentDuration;
+      
+        // Renter rents the item
+        await marketplace.connect(renter).rentOutToken(newTokenId, expires, { value: rentPrice });
 
-        const totalForSale = await marketplace.fetchMarketItems();
-
-        // console.log(totalForSale)
-        expect(totalForSale.length).to.equal(2);
-
+        // Check if the token is rented out correctly
+        let userOfToken = await marketplace.userOf(newTokenId);
+        expect(userOfToken).to.equal(renter.address);
+      
+        // Advance the blockchain time by more than 24 hours to simulate expiry
+        await ethers.provider.send('evm_increaseTime', [rentDuration + 1]); // +1 to ensure we're past the expiry
+        await ethers.provider.send('evm_mine'); // mine a new block to make sure the time change takes effect
+      
+        userOfToken = await marketplace.userOf(newTokenId);
+        expect(userOfToken).to.equal(ethers.constants.AddressZero, "The token should not have an active user after expiry");
       });
+
+    it("Should allow users to rent out tokens", async function () {
+        // Seller lists an item for rent
+        const rentPrice = ethers.utils.parseEther("0.001"); // Rent price set by the seller
+        await marketplace.connect(seller).createToken("tokenURI", ethers.utils.parseEther("0.1"), rentPrice, true, true, false, { value: listingPrice });
+        const newTokenId = await marketplace._tokenIds();
+      
+        // Set the expiration time for the rent
+        const rentDuration = 24 * 60 * 60; // 24 hours in seconds
+        const expires = (await ethers.provider.getBlock('latest')).timestamp + rentDuration;
+      
+        // Renter rents the item
+        await marketplace.connect(renter).rentOutToken(newTokenId, expires, { value: rentPrice });
+
+        // Check if the token is rented out correctly
+        let userOfToken = await marketplace.userOf(newTokenId);
+        expect(userOfToken).to.equal(renter.address);
+      
+        // Advance the blockchain time by less than 24 hours to simulate expiry
+        await ethers.provider.send('evm_increaseTime', [rentDuration - 1]); // +1 to ensure we're past the expiry
+        await ethers.provider.send('evm_mine'); // mine a new block to make sure the time change takes effect
+      
+        userOfToken = await marketplace.userOf(newTokenId);
+        expect(userOfToken).to.equal(renter.address, "The token should have an active user before expiry");
+      });
+
+      // it("Should fetch and return all available NFTs for sale", async function () {
+      //   // Seller lists an item
+      //   await marketplace.connect(seller).createToken("tokenURI", ethers.utils.parseEther("0.1"), ethers.utils.parseEther("0.001"), true, true, false, { value: listingPrice });
+      //   const newTokenId1 = await marketplace._tokenIds();
+
+      //   await marketplace.connect(seller).createToken("tokenURI2", ethers.utils.parseEther("0.1"), ethers.utils.parseEther("0.001"), true, true, false, { value: listingPrice });
+      //   const newTokenId2 = await marketplace._tokenIds();
+      
+      //   await marketplace.connect(seller).createToken("tokenURI2", ethers.utils.parseEther("0.1"), ethers.utils.parseEther("0.001"), true, true, false, { value: listingPrice });
+      //   // const newTokenId2 = await marketplace._tokenIds();
+      
+      
+      //   // Buyer purchases an item
+      //   await marketplace.connect(buyer).createMarketSale(newTokenId1, { value: ethers.utils.parseEther("0.1") });
+      //   const item = await marketplace.idToMarketItem(newTokenId1);
+
+      //   const totalForSale = await marketplace.fetchMarketItems();
+
+      //   // console.log(totalForSale)
+      //   expect(totalForSale.length).to.equal(2);
+
+      // });
       
       // it("Should fetch and return user owner NFTs", async function() {
 
