cdde120bef86bec33b3be35ff664cd70ecf95a7d
dhiraj0911 <djirajbari123@gmail.com>
2024-01-25 10:01:59 +0530

    ok

--- context/constants.js
+++ context/constants.js
@@ -1,14 +1,5 @@
-// import market from './NFTMarketplace.json';
-
-
 import rentableMarketPlace from './RentableNFTMarketplace.json';
 
-// export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 export const MarketAddress = '0x9458CAC87BeF5c5ed62d8666466FAa3354A2dc89';
 
 export const MarketAddressABI = rentableMarketPlace.abi;
-
-
-
-// export const RentableNFTMarketplaceAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-// export const RentableNFTMarketplaceABI = rentableMarketPlace.abi;
