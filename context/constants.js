fc8165b85ce43dffa83a9b82514d397c416b33a3
dhiraj0911 <djirajbari123@gmail.com>
2024-02-10 17:53:01 +0530

    update address

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import rentableMarketPlace from './RentableNFTMarketplace.json';
 
-export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const MarketAddress = '0x1bD31601e6A18D63b915ac07244Af2fFF2A5A35c';
 export const MarketAddressABI = rentableMarketPlace.abi;
