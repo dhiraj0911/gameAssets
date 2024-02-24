232e88717bf4919012c06431c09725744e2c21b9
dhiraj0911 <djirajbari123@gmail.com>
2024-02-24 17:25:45 +0530

    fix rentCard

--- components/RentCard.jsx
+++ components/RentCard.jsx
@@ -5,12 +5,16 @@ import {
   useConnectionStatus,
   useDisconnect,
   useAddress,
-} from "@thirdweb-dev/react"; 
+} from "@thirdweb-dev/react";
 
 const RentCard = ({ nft, onProfilePage }) => {
   const { nftCurrency } = useContext(NFTContext);
   const ownerAddress = nft.owner.toLowerCase();
-  const currentAccountAddress = useAddress();
+  const currentAccountAddress = useAddress().toLowerCase();
+  const renterAddress =
+    nft.rented === true || nft.rented === "true"
+      ? nft.renter.toLowerCase()
+      : null;
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
@@ -36,7 +40,10 @@ const RentCard = ({ nft, onProfilePage }) => {
   }, [nft.expires]);
 
   return (
-    <div className="flex flex-col rounded-3xl border-solid hover:border-dotted m-3" style={{ backgroundColor: '#011627 ' }}>
+    <div
+      className="flex flex-col rounded-3xl border-solid hover:border-dotted m-3"
+      style={{ backgroundColor: "#011627 " }}
+    >
       <div className="px-4 py-7 sm:p-10 sm:pb-6">
         <div className="grid items-center justify-center w-full grid-cols-1 text-left">
           <div>
@@ -45,9 +52,11 @@ const RentCard = ({ nft, onProfilePage }) => {
             </h2>
             <p className="mt-6 text-sm text-gray-100">ID: {nft.id}</p>
           </div>
-          {(ownerAddress === currentAccountAddress && !nft.rented) ? (
-              <></>
-          ): (ownerAddress === currentAccountAddress && nft.rented) ? (
+          {ownerAddress === currentAccountAddress && !nft.rented ? (
+            <></>
+          ) : ownerAddress === currentAccountAddress && nft.rented ? (
+            <>Your asset is rented by somebody</>
+          ) : renterAddress === currentAccountAddress && nft.rented ? (
             <div className="flex gap-5 mt-4 text-center">
               <div className="flex flex-col">
                 <span className="countdown font-mono text-4xl">
@@ -75,43 +84,41 @@ const RentCard = ({ nft, onProfilePage }) => {
               </div>
             </div>
           ) : nft.rented ? (
-              <>
-                This NFT is already rented.
-              </>
+            <>This NFT is already rented.</>
           ) : (
-              <>
-                {nft.forRent ? (
+            <>
+              {nft.forRent ? (
                 <div className="mt-6">
                   <p>
                     <span className="text-5xl font-light tracking-tight text-white">
                       {nft.rentPrice}
                     </span>
-                    <span className="text-base font-medium text-white"> {nftCurrency(nft)} /day </span>
+                    <span className="text-base font-medium text-white">
+                      {" "}
+                      {nftCurrency(nft)} /day{" "}
+                    </span>
                   </p>
                 </div>
               ) : (
-                <div className="mt-6">
-                  <p>
-                    <span className="text-5xl font-light tracking-tight text-white">
-                      {nft.price}
-                    </span>
-                    <span className="text-base font-medium text-white"> {nftCurrency(nft)}</span>
-                  </p>
-                </div>
+                <></>
               )}
             </>
           )}
         </div>
       </div>
-      <Link href={{ pathname: '/nft-details', query: nft }}>
+      <Link href={{ pathname: "/nft-details", query: nft }}>
         <div className="flex px-6 pb-8 sm:px-8">
-          <a aria-describedby="tier-starter" className="items-center justify-center w-full px-6 py-2.5 text-center text-black duration-200 bg-white border-2 border-white rounded-full inline-flex hover:bg-transparent hover:border-white hover:text-white focus:outline-none focus-visible:outline-white text-sm focus-visible:ring-white" href="#">
+          <a
+            aria-describedby="tier-starter"
+            className="items-center justify-center w-full px-6 py-2.5 text-center text-black duration-200 bg-white border-2 border-white rounded-full inline-flex hover:bg-transparent hover:border-white hover:text-white focus:outline-none focus-visible:outline-white text-sm focus-visible:ring-white"
+            href="#"
+          >
             View More
           </a>
         </div>
       </Link>
     </div>
-  )
+  );
 };
 
 export default RentCard;
